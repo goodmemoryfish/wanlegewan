a7ee81309bddf234ab7e8e78dee8b550de35be03
goodmemoryfish <131885306+goodmemoryfish@users.noreply.github.com>
2023-04-26 22:06:58 +0800

    Add files via upload

---
+++ 羊了个羊代码【完善版】/sheep/src/com/bjsxt/model/Brand.java
@@ -0,0 +1,152 @@
+package com.bjsxt.model;
+
+
+/*
+    对于游戏当中  牌 的抽象。  比如 胡萝卜，玉米， 青草，
+
+
+ */
+
+
+import com.bjsxt.Start;
+import com.bjsxt.TestRenderMap;
+import com.bjsxt.util.ImageUtil;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.UUID;
+
+
+public class Brand extends Component{
+
+
+    private  String  name;          // 对当前的图案进行命名  可以用来定位  图片的文件
+    private  String  id;
+
+    private  Integer x=0;
+    private  Integer y=0;
+    private  int width=50;
+    private  int height=50;
+
+    private Image   image;
+
+    private Image   grayImage;
+
+
+    private Boolean isGray=false;    //  是否置灰
+
+    private Cell   cell;
+
+
+
+
+
+
+
+
+    public Brand() {
+    }
+
+    public Brand(String name) {
+        this.name = name;
+        this.image = ImageUtil.get(name+".png");
+        this.grayImage = ImageUtil.get(name+"_gray.png");
+
+        this.id= UUID.randomUUID().toString();
+
+        Brand  self=this;
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                Brand  brand = (Brand) e.getSource();
+                if(brand.getGray()||brand.getName().equals("消除区域")||brand.getName().equals("背景草地")){
+
+                    return;
+                }else{
+                    System.out.println(brand.getName()+"被点击啦.....");
+
+//                    self.getParent().remove(self);
+
+
+                    eliminatebox.addSlot(brand);
+
+
+                    self.getCell().setState(1);
+                    self.getCell().setBrand(null);
+                    self.setCell(null);
+
+                    Start.map.grayDecide();
+
+
+                }
+
+            }
+        });
+
+
+    }
+
+    Eliminatebox eliminatebox  =new Eliminatebox();
+
+    @Override
+    public void paint(Graphics g) {
+//        super.paint(g);
+
+        if(isGray){
+            g.drawImage(this.getGrayImage(),x,y,null);
+
+        }else{
+
+            g.drawImage(this.getImage(),x,y,null);
+
+        }
+    }
+
+
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    public Boolean getGray() {
+        return isGray;
+    }
+
+    public void setGray(Boolean gray) {
+        isGray = gray;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Image getGrayImage() {
+        return grayImage;
+    }
+
+    public void setGrayImage(Image grayImage) {
+        this.grayImage = grayImage;
+    }
+
+
+}
